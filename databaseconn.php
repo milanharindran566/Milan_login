80ad5bc0193c4774ce5872b05eaf8376364c610b
milanharindran566 <44251616+milanharindran566@users.noreply.github.com>
2020-05-16 13:53:48 +0530

    Create databaseconn.php
    database config file

---
+++ databaseconn.php
@@ -0,0 +1,7 @@
+ <?php
+ $dbservername = "localhost";
+ $dbusername = "root";
+ $dbpassword = "123456";
+ $dbname = "users";
+ $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
+ ?>
